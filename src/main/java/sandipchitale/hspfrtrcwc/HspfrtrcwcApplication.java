4c081be3c3a45c45ad99f2561bc44f30880651fd
SANDIP CHITALE <sandipchitale@gmail.com>
2023-08-30 12:50:26 -0700

    Initial commit.

---
+++ src/main/java/sandipchitale/hspfrtrcwc/HspfrtrcwcApplication.java
@@ -0,0 +1,123 @@
+package sandipchitale.hspfrtrcwc;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.client.support.RestTemplateAdapter;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
+
+@SpringBootApplication
+public class HspfrtrcwcApplication {
+
+	record Post(long id, long userid, String title, boolean completed) {}
+
+	@Bean
+	@Qualifier("post-web-client")
+	WebClient postWebClient(WebClient.Builder builder) {
+		return builder.baseUrl("https://jsonplaceholder.typicode.com/posts").build();
+	}
+
+	interface PostService {
+		@GetExchange
+		List<Post> getPosts();
+
+		@GetExchange("/{id}")
+		Post getPost(@PathVariable("id") long id);
+	}
+
+	@Bean
+	PostService postClient(@Qualifier("post-web-client") WebClient webClient) {
+		HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builderFor(WebClientAdapter.forClient(webClient)).build();
+//		HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builderFor(WebClientAdapter.create(webClient)).build();
+		return proxyFactory.createClient(PostService.class);
+	}
+
+
+	@Bean
+	CommandLineRunner clrPostService(PostService postService) {
+		return (String... args) -> {
+			System.out.println("Post using PostClient: " + postService.getPosts());
+			System.out.println("Post using PostClient: " + postService.getPost(1));
+		};
+	}
+
+	record Todo(long id, String name) {}
+
+	@Bean
+	@Qualifier("todo-rest-client")
+	RestClient todoRestClient(RestClient.Builder builder) {
+		return builder.baseUrl("https://jsonplaceholder.typicode.com/todos").build();
+	}
+
+	interface TodoService {
+		@GetExchange
+		List<Todo> getTodos();
+
+		@GetExchange("/{id}")
+		Todo getTodo(@PathVariable("id") long id);
+	}
+
+	@Bean
+	TodoService todoService(@Qualifier("todo-rest-client") RestClient restClient) {
+		HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return proxyFactory.createClient(TodoService.class);
+	}
+
+	@Bean
+	CommandLineRunner clrTodoService(TodoService todoService) {
+		return (String... args) -> {
+			System.out.println("Todo using TodoClient: " + todoService.getTodos());
+			System.out.println("Todo using TodoClient: " + todoService.getTodo(1));
+		};
+	}
+
+	record Comment(long id, String name) {}
+
+	@Bean
+	@Qualifier("comment-rest-template")
+	RestTemplate commentRestTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@HttpExchange("https://jsonplaceholder.typicode.com/comments")
+	interface CommentService {
+		@GetExchange
+		List<Comment> getComments();
+
+		@GetExchange("/{id}")
+		Comment getComment(@PathVariable("id") long id);
+	}
+
+	@Bean
+	CommentService commentService(@Qualifier("comment-rest-template") RestTemplate restTemplate) {
+		HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builderFor(RestTemplateAdapter.create(restTemplate)).build();
+		return proxyFactory.createClient(CommentService.class);
+	}
+
+
+	@Bean
+	CommandLineRunner clrCommentService(CommentService commentService) {
+		return (String... args) -> {
+			System.out.println("Comment using CommentClient: " + commentService.getComment(1));
+			System.out.println("Comment using CommentClient: " + commentService.getComments());
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HspfrtrcwcApplication.class, args);
+	}
+
+}
